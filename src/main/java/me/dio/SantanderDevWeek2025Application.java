4748ab5946dac84fd9f8049ee94e8461c49bc96e
yohana-teixeira01 <yteixeira133@gmail.com>
2025-02-15 11:08:47 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderDevWeek2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2025Application.class, args);
+	}
+
+}
