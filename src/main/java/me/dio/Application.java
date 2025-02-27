971afeb18da045848b7f79ac8ddd7797ea5136f4
yohana-teixeira01 <yteixeira133@gmail.com>
2025-02-27 12:07:04 -0300

    Tentando resolver o bug no swagger

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
-@SpringBootApplication(scanBasePackages = "me.dio")
+@SpringBootApplication()
 public class Application {
 
 	public static void main(String[] args) {
